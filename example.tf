cb01525676da6380983e562cd7957836623336fc
t-mario-y <t.mario.yoshi@gmail.com>
2020-02-03 11:50:27 +0900

    terraform sample code

---
+++ example.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = var.region
+}
+
+# create a new AWS instance
+resource "aws_instance" "example" {
+  ami           = "ami-b374d5a5"
+  instance_type = "t2.miccro"
+}
+
+resource "aws_eip" "ip" {
+  vpc      = true
+  instance = aws_instance.example.id
+}
