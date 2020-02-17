926864988b4cf92fd14164be48dda3ed7d20aadd
t-mario-y <t.mario.yoshi@gmail.com>
2020-02-17 16:15:04 +0900

    chage code formatting rule(IntelliJ Idea)

--- example.tf
+++ example.tf
@@ -3,17 +3,16 @@ variable "region" {
 }
 
 provider "aws" {
-  profile = "default"
-  region  = var.region
+  region = var.region
 }
 
 # create a new AWS instance
 resource "aws_instance" "example" {
-  ami           = "ami-b374d5a5"
+  ami = "ami-b374d5a5"
   instance_type = "t2.micro"
 }
 
 resource "aws_eip" "ip" {
-  vpc      = true
+  vpc = true
   instance = aws_instance.example.id
 }
